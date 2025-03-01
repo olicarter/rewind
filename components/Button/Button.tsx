32e73c5c024e4e4a7bd26402c900561365183ed5
Oli Carter <olicartr@gmail.com>
2025-03-01 13:36:53 +0200

    refactor: Simplify Button component and update CSS styles

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -3,24 +3,12 @@ import { ComponentPropsWithoutRef } from 'react'
 import { cn } from '@/utils'
 import styles from './Button.module.css'
 
-
 interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
   asChild?: boolean
-  secondary?: boolean
 }
 
-export function Button({
-  asChild,
-  className,
-  secondary,
-  ...props
-}: ButtonProps) {
+export function Button({ asChild, className, ...props }: ButtonProps) {
   const Comp = asChild ? Slot.Root : 'button'
 
-  return (
-    <Comp
-      className={cn(styles.button, secondary && styles.secondary, className)}
-      {...props}
-    />
-  )
+  return <Comp className={cn(styles.button, className)} {...props} />
 }
