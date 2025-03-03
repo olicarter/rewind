e0af113fcd22f13f991f071f2503849eae7183a2
Oli Carter <olicartr@gmail.com>
2025-03-03 19:22:54 +0200

    refactor: Extract Group component, update styling, and fix group deletion

---
+++ app/rooms/[roomId]/Group.tsx
@@ -0,0 +1,91 @@
+import { useDroppable } from '@dnd-kit/core'
+import { ChangeEvent, useState } from 'react'
+import {
+  db,
+  type Group,
+  Meeting,
+  PostWithAuthor,
+  Profile,
+  Stage,
+} from '@/app/db'
+import { Button } from '@/components/Button'
+import { TextInput } from '@/components/TextInput'
+import styles from './Group.module.css'
+import { Post } from './Post'
+
+export interface GroupProps {
+  group: Group
+  meeting: Meeting
+  posts: PostWithAuthor[]
+  profile: Profile
+}
+
+export function Group(props: GroupProps) {
+  const [uuid] = useState(() => crypto.randomUUID())
+  const id = props.group.id || uuid
+
+  const droppable = useDroppable({ id, data: { type: 'group' } })
+
+  const isDraggingPostInGroup = props.posts.some(
+    post => post.id === droppable.active?.id
+  )
+
+  async function toggleVote() {
+    const votedBy = new Set<string>(JSON.parse(props.group.votedBy ?? '[]'))
+    const newVotedBy = votedBy.has(props.profile.id)
+      ? Array.from(votedBy).filter(id => id !== props.profile.id)
+      : [...votedBy, props.profile.id]
+    await db.transact([
+      db.tx.groups[props.group.id].update({
+        votedBy: JSON.stringify(newVotedBy),
+      }),
+    ])
+  }
+
+  async function updateGroupName(event: ChangeEvent<HTMLInputElement>) {
+    const name = event.target.value
+    await db.transact([db.tx.groups[props.group.id].update({ name })])
+  }
+
+  return (
+    <li
+      className={styles.group}
+      id={id}
+      ref={droppable.setNodeRef}
+      style={{
+        opacity: droppable.isOver && !isDraggingPostInGroup ? 0.5 : 1,
+      }}
+    >
+      <header className={styles.header}>
+        <TextInput
+          className={styles.groupName}
+          defaultValue={props.group.name}
+          onChange={updateGroupName}
+          readOnly={props.meeting.stage !== Stage.Group}
+        />
+        {props.meeting.stage === Stage.Discussion && (
+          <Button
+            className={styles.voteButton}
+            onClick={toggleVote}
+            type="button"
+          >
+            {props.group.votedBy?.includes(props.profile.id)
+              ? 'Unvote'
+              : 'Vote'}
+          </Button>
+        )}
+      </header>
+      <ul>
+        {props.posts.map(post => (
+          <Post
+            key={post.id}
+            meetingId={props.meeting.id}
+            meetingStage={props.meeting.stage}
+            post={post}
+            profile={props.profile}
+          />
+        ))}
+      </ul>
+    </li>
+  )
+}
--- app/rooms/[roomId]/Posts.tsx
+++ app/rooms/[roomId]/Posts.tsx
@@ -3,17 +3,9 @@ import { restrictToWindowEdges } from '@dnd-kit/modifiers'
 import { id } from '@instantdb/react'
 import { groupBy } from 'lodash'
 import { registerMasonry } from 'masonry-pf'
-import { ChangeEvent, useMemo, useState } from 'react'
-import {
-  db,
-  type Group,
-  Meeting,
-  PostWithAuthor,
-  Profile,
-  Stage,
-} from '@/app/db'
-import { Button } from '@/components/Button'
-import { TextInput } from '@/components/TextInput'
+import { useMemo } from 'react'
+import { db, Meeting, PostWithAuthor, Profile, Stage } from '@/app/db'
+import { Group } from './Group'
 import { Post } from './Post'
 import styles from './Posts.module.css'
 
@@ -39,6 +31,8 @@ export function Posts(props: PostsProps) {
             post.author && props.selectedProfileIds.includes(post.author.id)
           )
         })
+      case Stage.Feedback:
+        return props.posts
       default:
         return []
     }
@@ -49,7 +43,11 @@ export function Posts(props: PostsProps) {
     props.selectedProfileIds,
   ])
 
-  if ([Stage.Group, Stage.Discussion].includes(props.meeting.stage)) {
+  if (
+    [Stage.Group, Stage.Discussion, Stage.Feedback].includes(
+      props.meeting.stage
+    )
+  ) {
     return (
       <DndContext
         modifiers={[restrictToWindowEdges]}
@@ -62,51 +60,53 @@ export function Posts(props: PostsProps) {
           // If the post is being dropped on itself, do nothing
           if (!dropId || dragPostId === dropId) return
 
-          // If the post is being dropped on the background, remove it from any group
+          const newGroupId = id()
+
+          const postsInDragGroup = props.posts.filter(
+            post => post.group?.id === dragGroupId
+          )
+
+          // If the post is being dropped on the background
           if (dropType === 'posts' && dragGroupId) {
             // Don't do anything if the group has only one post
-            if (
-              props.posts.filter(post => post.group?.id === dragGroupId)
-                .length === 1
-            ) {
-              return
-            }
+            if (postsInDragGroup.length === 1) return
 
-            // Create a new group and add the post to it
-            const newGroupId = id()
-            await db.transact([
+            // Remove the post from the group and create a new group
+            const actions = [
               db.tx.posts[dragPostId].unlink({ group: dragGroupId }),
               db.tx.groups[newGroupId].update({ name: 'Group' }),
               db.tx.posts[dragPostId].link({ group: newGroupId }),
-            ])
+            ]
 
             // If the group is now empty, delete it
-            if (
-              !props.posts.filter(post => post.group?.id === dragGroupId).length
-            ) {
-              await db.transact([db.tx.groups[dragGroupId].delete()])
+            if (postsInDragGroup.length === 0) {
+              actions.push(db.tx.groups[dragGroupId].delete())
             }
+
+            await db.transact(actions)
           }
 
-          // If the post is being dropped on a post, create a new group and add both posts to it
+          // If the post is being dropped on a post
           if (dropType === 'post') {
-            const groupId = id()
-            db.transact([
-              db.tx.groups[groupId].update({ name: 'Group' }),
-              db.tx.posts[dragPostId].link({ group: groupId }),
-              db.tx.posts[dropId].link({ group: groupId }),
+            // Create a new group and add both posts to it
+            await db.transact([
+              db.tx.groups[newGroupId].update({ name: 'Group' }),
+              db.tx.posts[dragPostId].link({ group: newGroupId }),
+              db.tx.posts[dropId].link({ group: newGroupId }),
             ])
           }
 
-          // If the post is being dropped on a group, add the post to the group
+          // If the post is being dropped on a group
           if (dropType === 'group') {
-            db.transact([
-              ...(props.posts.filter(post => post.group?.id === dragGroupId)
-                .length === 1
-                ? [db.tx.groups[dragGroupId].delete()]
-                : []),
-              db.tx.posts[dragPostId].link({ group: dropId }),
-            ])
+            // Add the post to the group
+            const actions = [db.tx.posts[dragPostId].link({ group: dropId })]
+
+            // If the old group is empty, delete it
+            if (postsInDragGroup.length === 1 && dragGroupId !== dropId) {
+              actions.push(db.tx.groups[dragGroupId].delete())
+            }
+
+            await db.transact(actions)
           }
         }}
       >
@@ -186,82 +186,5 @@ function GroupPostsList(props: GroupPostsListProps) {
         />
       ))}
     </ul>
-  )
-}
-
-interface GroupProps {
-  group: Group
-  meeting: Meeting
-  posts: PostWithAuthor[]
-  profile: Profile
-}
-
-function Group(props: GroupProps) {
-  const [uuid] = useState(() => crypto.randomUUID())
-  const id = props.group.id || uuid
-
-  const droppable = useDroppable({ id, data: { type: 'group' } })
-
-  const isDraggingPostInGroup = props.posts.some(
-    post => post.id === droppable.active?.id
-  )
-
-  async function toggleVote() {
-    const votedBy = new Set<string>(JSON.parse(props.group.votedBy ?? '[]'))
-    const newVotedBy = votedBy.has(props.profile.id)
-      ? Array.from(votedBy).filter(id => id !== props.profile.id)
-      : [...votedBy, props.profile.id]
-    await db.transact([
-      db.tx.groups[props.group.id].update({
-        votedBy: JSON.stringify(newVotedBy),
-      }),
-    ])
-  }
-
-  async function updateGroupName(event: ChangeEvent<HTMLInputElement>) {
-    const name = event.target.value
-    await db.transact([db.tx.groups[props.group.id].update({ name })])
-  }
-
-  return (
-    <li
-      className={styles.group}
-      id={id}
-      ref={droppable.setNodeRef}
-      style={{
-        opacity: droppable.isOver && !isDraggingPostInGroup ? 0.5 : 1,
-      }}
-    >
-      <header className={styles.header}>
-        <TextInput
-          className={styles.groupName}
-          defaultValue={props.group.name}
-          onChange={updateGroupName}
-          readOnly={props.meeting.stage !== Stage.Group}
-        />
-        {props.meeting.stage === Stage.Discussion && (
-          <Button
-            className={styles.voteButton}
-            onClick={toggleVote}
-            type="button"
-          >
-            {props.group.votedBy?.includes(props.profile.id)
-              ? 'Unvote'
-              : 'Vote'}
-          </Button>
-        )}
-      </header>
-      <ul>
-        {props.posts.map(post => (
-          <Post
-            key={post.id}
-            meetingId={props.meeting.id}
-            meetingStage={props.meeting.stage}
-            post={post}
-            profile={props.profile}
-          />
-        ))}
-      </ul>
-    </li>
   )
 }
